ff852e77ba2100be849c9fba452f336d176de03b
Naginipython <naginipython332@gmail.com>
2023-11-19 08:58:58 -0500

    Small change

--- ex-01-math-api/src/math_router.rs
+++ ex-01-math-api/src/math_router.rs
@@ -1,6 +1,6 @@
 use axum::extract::Path;
 
-pub async fn math_route(Path((a, b)): Path<(i32, i32)>) -> String{
+pub async fn math_route(Path((a, b)): Path<(i32, i32)>) -> String {
     let string_ans: String = (a + b).to_string();
     string_ans
 }
