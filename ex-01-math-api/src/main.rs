e2dda8fd6fb59d4e85c1a62c4d0a4405d25ac06a
Naginipython <naginipython332@gmail.com>
2023-11-19 08:54:03 -0500

    Finished ex-01

--- ex-01-math-api/src/main.rs
+++ ex-01-math-api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+pub mod router;
+pub mod math_router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(router::root))
+        .route("/about", get(router::about))
+        .route("/add/:a/:b", get(math_router::math_route));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    println!("Server is running on localhost:3000");
 }
---
+++ ex-01-math-api/src/router.rs
@@ -0,0 +1,7 @@
+pub async fn root() -> &'static str {
+    "Welcome to the Axum Router Excerise"
+}
+
+pub async fn about() -> &'static str {
+    "This is the about page"
+}
