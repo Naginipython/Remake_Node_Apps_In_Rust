d0f4fdd105ff6830dfa480d478d66e2ce88858f2
Naginipython <naginipython332@gmail.com>
2023-11-21 08:56:18 -0500

    added todos

--- ex-04-cocktails/src/routes/mod.rs
+++ ex-04-cocktails/src/routes/mod.rs
@@ -15,10 +15,12 @@ pub async fn hello() -> Json<Value> {
     Json(json!({ "message": "hello world" }))
 }
 
+// POSSIBLY MAKE DRINK AN option<String> SO IT IS ALLOWED TO FAIL?
 pub async fn cocktail(extract::Json(data): extract::Json<Drink>) -> Json<Value> {
     // I get a valid response OR an empty String
     let res: Value = modules::get_cocktails(data.drink).await.unwrap_or_else(|_| {
         serde_json::Value::String(String::from(""))
     });
+    // Extract only the strDrink, and make into a Vec. Return the Vec as JSON.
     Json(json!(res))
 }
