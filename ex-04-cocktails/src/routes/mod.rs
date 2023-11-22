92998f4f8dc88734679f1b963a5d09f40963f6a4
Naginipython <naginipython332@gmail.com>
2023-11-21 20:26:35 -0500

    Finished ex-04-cocktails

--- ex-04-cocktails/src/routes/mod.rs
+++ ex-04-cocktails/src/routes/mod.rs
@@ -7,6 +7,10 @@ use serde_json::{ Value, json };
 use crate::modules;
 
 #[derive(Deserialize, Debug)]
+pub struct DrinkExtract {
+    drink: Option<String>,
+}
+
 pub struct Drink {
     drink: String,
 }
@@ -15,12 +19,24 @@ pub async fn hello() -> Json<Value> {
     Json(json!({ "message": "hello world" }))
 }
 
-// POSSIBLY MAKE DRINK AN option<String> SO IT IS ALLOWED TO FAIL?
-pub async fn cocktail(extract::Json(data): extract::Json<Drink>) -> Json<Value> {
-    // I get a valid response OR an empty String
-    let res: Value = modules::get_cocktails(data.drink).await.unwrap_or_else(|_| {
-        serde_json::Value::String(String::from(""))
-    });
-    // Extract only the strDrink, and make into a Vec. Return the Vec as JSON.
-    Json(json!(res))
+pub async fn cocktail(extract::Json(data): extract::Json<DrinkExtract>) -> Json<Value> {
+    // Note: With this, a user CAN send incorrect JSON, but it will not return anything interesting.
+    let data: Drink = Drink {
+        drink: data.drink.map_or(String::from(""), |v| v),
+    };
+    let mut result: Vec<String> = Vec::new();
+
+    if data.drink.ne("") {
+        let res: Value = modules::get_cocktails(data.drink).await.unwrap_or_default();
+    
+        let or_else: Vec<Value> = vec![json!([])];      // This will unwrap, or use a empty Vec<Value> (aka, loop doesn't happen)
+        for i in 0..res["drinks"].as_array().unwrap_or_else(|| &or_else).len() {
+            let val: String = String::from( res["drinks"][i]["strDrink"].as_str().unwrap_or_default() );
+            if val.ne("") {
+                result.push(val);
+            }
+        }
+    }
+    
+    Json(json!(result))
 }
